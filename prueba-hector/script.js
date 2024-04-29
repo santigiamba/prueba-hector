db5694b6a43158ce3e352ac409a1f59703eed678
santigiamba <112349844+santigiamba@users.noreply.github.com>
2024-04-29 10:59:52 -0300

    aca esta
    me falto varias cosas

---
+++ prueba-hector/script.js
@@ -0,0 +1,8 @@
+const playButton = document.getElementById('playButton');
+
+playButton.addEventListener('click', () => {
+    const boton = document.querySelector('.boton.boton1');
+    boton.style.opacity = '0.5';
+});
+
+
